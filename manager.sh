8f34d0b8fc8775a780007ad721bb4b2d0d9e1075
Hamse Abdi Hashi <hamseah@stud.ntnu.no>
2019-02-13 13:17:19 +0100

    some fixes

--- manager.sh
+++ manager.sh
@@ -2,6 +2,8 @@
 # tempdeb=$(mktemp /tmp/debpackage.XXXXXXXXXXXXXXXXXX) || exit 1
 
 # For manager node
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBJRiXTpatyOCwAvb9ZRyNfquKJdpLJVkuYD4sMCAI1MiACPqQMos0e2efEu/fzCgNluIg5L5tERsweBzTtQraTRTgXhwaP1Nj5lDaSfiFBlx5KzPm9fsN9KMB3yxbX8xraMLrK/xNjgy+gqX2G0dwnY4/EUdj+npRNzf3uBY6jFaw3FD/Q1LiqA8rE9gRzKQOD8EbBF++3A+FYx/fzrL9Dx6td1K6IoWBA/O8azCYhYMf+HEmxCKA36yabGv97XmNxX8aDJPGGYePdXIZQ0lpOWZpP6zRnm2cgOHLD7A84rzXjFlA0uqVhwRjEJzvsnsnlXGl6c9MsioxZsHnQ3eD morten@morten-VirtualBox" >> /home/ubuntu/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVcBkZbQP//tgjY/lrk2HxhYiJs8c7PricTWmQDMb9T2qRAhwIw6kAUo0l8fVDZ+NLMGaHfQPY1mBnBUbiWTzNSPaNPlpQryXBon6aMGFAdypK3fjQHi9PgAtqRjqOxpwA8TYIfy6k4gqDyRerlnHmmaK69IBO2Dwjq9G92Cx9lfIQEJhogsaZUInVa090+lZuIHto/3GacOQixYVxeGDLd1X0Y+QGK72T1V2dzbZBu6hXifTVcBjsVFkT4O+6KtxIkOH0mJQBwLNuzF0hydKxNn7aDt37gzYdjz/yRD1PjRs9dN8eeRn4CorzR+PUJOjWW2xQXY/gaeDVbCdAxQed fredros@localhost.localdomain" >> /home/ubuntu/.ssh/authorized_keys
 cd /root/
 if [ $(pwd) -ne '/root']; then
 	echo "we are not in root folder, instead we are in $(pwd)" >> /home/ubuntu/log.txt
@@ -21,16 +23,28 @@ echo "apt-get install -y python-dev libffi-dev gcc libssl-dev python-selinux pyt
 # Install ansible
 echo **INSTALLING ANSIBLE**
 echo **INSTALLING ANSIBLE**
+sudo apt-get -y install ansible
+echo $(ansible --version) >> /home/ubuntu/log.txt
+
 sudo pip install -U ansible==2.4
 echo "pip install -U ansible==2.4" >> /home/ubuntu/log.txt
-sudo apt-get install -y ansible==2.4
-echo "apt-get install -y ansible==2.4"  >> /home/ubuntu/log.txt
 sudo -H pip install --ignore-installed PYYAML
 echo "sudo -H pip install --ignore-installed PYYAML" >> /home/ubuntu/log.txt
+
+echo "[defaults]
+host_key_checking=False
+pipelining=True
+forks=100" >> /etc/ansible/ansible.cfg
+echo "kopierte ting til ansible.cfg" >> /home/ubuntu/log.txt
+
 sudo pip install kolla-ansible
 echo "pip install kolla-ansible" >> /home/ubuntu/log.txt
+sudo apt-get install -y ansible==2.4
+
+echo $(ansible --version) >> /home/ubuntu/log.txt
 sudo apt update 
 echo "apt update" >> /home/ubuntu/log.txt
+
 # moving things around
 echo *****MOVING THINGS ARROUND***** >> /home/ubuntu/log.txt
 cp -r /usr/local/share/kolla-ansible/etc_examples/kolla /etc/
@@ -86,8 +100,6 @@ sudo apt-get -y install ntp
 echo "install ntp" >> /home/ubuntu/log.txt
 sudo service libvirt-bin stop
 sudo update-rc.d libvirt-bin disable
-
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBJRiXTpatyOCwAvb9ZRyNfquKJdpLJVkuYD4sMCAI1MiACPqQMos0e2efEu/fzCgNluIg5L5tERsweBzTtQraTRTgXhwaP1Nj5lDaSfiFBlx5KzPm9fsN9KMB3yxbX8xraMLrK/xNjgy+gqX2G0dwnY4/EUdj+npRNzf3uBY6jFaw3FD/Q1LiqA8rE9gRzKQOD8EbBF++3A+FYx/fzrL9Dx6td1K6IoWBA/O8azCYhYMf+HEmxCKA36yabGv97XmNxX8aDJPGGYePdXIZQ0lpOWZpP6zRnm2cgOHLD7A84rzXjFlA0uqVhwRjEJzvsnsnlXGl6c9MsioxZsHnQ3eD morten@morten-VirtualBox" >> /home/ubuntu/.ssh/authorized_keys
 
 echo "Done!!" >> /home/ubuntu/log.txt
 
--- node.sh
+++ node.sh
@@ -5,20 +5,25 @@ apt-get -y update
 apt-get install -y python-pip
 pip install -U pip
 sudo -H pip install --ignore-installed PYYAML
-
-# Installing docker
-echo "installing docker" >> /home/ubuntu/log.txt
-sudo curl -sSL https://get.docker.io | bash
-sudo apt-get upgrade -y docker
-sudo apt-get update
-
-sudo apt-cache policy docker-ce
-sudo apt-get install -y docker-ce
-
-sudo apt-get upgrade docker-engine
-echo "sudo apt-get upgrade docker-engine" >>  /home/ubuntu/log.txt
-echo $(docker --version) >>  /home/ubuntu/log.txt
-sudo pip install -U docker-py
+#
+## Installing docker
+# MODIFISERT:
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudp apt update
+sudo apt-get -y install docker-ce="5:18.09.1~3-0~ubuntu-xenial" docker-ce-cli="5:18.09.1~3-0~ubuntu-xenial" containerd.io
+#echo "installing docker" >> /home/ubuntu/log.txt
+#sudo curl -sSL https://get.docker.io | bash
+#sudo apt-get upgrade -y docker
+#sudo apt-get update
+#
+#sudo apt-cache policy docker-ce
+#sudo apt-get install -y docker-ce
+#
+#sudo apt-get upgrade docker-engine
+#echo "sudo apt-get upgrade docker-engine" >>  /home/ubuntu/log.txt
+#echo $(docker --version) >>  /home/ubuntu/log.txt
+#sudo pip install -U docker-py
 sudo pip install -U docker
 sudo mkdir -p /etc/systemd/system/docker.service.d
 cat <<EOF > /etc/systemd/system/docker.service.d/kolla.conf
@@ -33,36 +38,38 @@ echo "install ntp" >> /home/ubuntu/log.txt
 sudo service libvirt-bin stop
 sudo update-rc.d libvirt-bin disable
 
-
-# Installing docker
-echo "installing docker" >> /home/ubuntu/log.txt
-sudo curl -sSL https://get.docker.io | bash
-sudo apt-get upgrade -y docker
-sudo apt-get update
-
-sudo apt-cache policy docker-ce
-sudo apt-get install -y docker-ce
-
-sudo apt-get upgrade docker-engine
-echo "sudo apt-get upgrade docker-engine" >>  /home/ubuntu/log.txt
-echo $(docker --version) >>  /home/ubuntu/log.txt
-sudo pip install -U docker-py
-sudo pip install -U docker
-sudo mkdir -p /etc/systemd/system/docker.service.d
-cat <<EOF > /etc/systemd/system/docker.service.d/kolla.conf
-[Service]
-MountFlags=shared
-EOF
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-
-sudo apt-get -y install ntp
-echo "install ntp" >> /home/ubuntu/log.txt
-sudo service libvirt-bin stop
-sudo update-rc.d libvirt-bin disable
-
-echo "Done!!" >> /home/ubuntu/log.txt
-echo "Done!!" >> /home/ubuntu/log.txt
+#
+## Installing docker
+#echo "installing docker" >> /home/ubuntu/log.txt
+#sudo curl -sSL https://get.docker.io | bash
+#sudo apt-get upgrade -y docker
+#sudo apt-get update
+#
+#sudo apt-cache policy docker-ce
+#sudo apt-get install -y docker-ce
+#
+#sudo apt-get upgrade docker-engine
+#echo "sudo apt-get upgrade docker-engine" >>  /home/ubuntu/log.txt
+#echo $(docker --version) >>  /home/ubuntu/log.txt
+#sudo pip install -U docker-py
+#sudo pip install -U docker
+#sudo mkdir -p /etc/systemd/system/docker.service.d
+#cat <<EOF > /etc/systemd/system/docker.service.d/kolla.conf
+#[Service]
+#MountFlags=shared
+#EOF
+#sudo systemctl daemon-reload
+#sudo systemctl restart docker
+#
+#sudo apt-get -y install ntp
+#echo "install ntp" >> /home/ubuntu/log.txt
+#sudo service libvirt-bin stop
+#sudo update-rc.d libvirt-bin disable
+#
+#echo "Done!!" >> /home/ubuntu/log.txt
+#echo "Done!!" >> /home/ubuntu/log.txt
 
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBJRiXTpatyOCwAvb9ZRyNfquKJdpLJVkuYD4sMCAI1MiACPqQMos0e2efEu/fzCgNluIg5L5tERsweBzTtQraTRTgXhwaP1Nj5lDaSfiFBlx5KzPm9fsN9KMB3yxbX8xraMLrK/xNjgy+gqX2G0dwnY4/EUdj+npRNzf3uBY6jFaw3FD/Q1LiqA8rE9gRzKQOD8EbBF++3A+FYx/fzrL9Dx6td1K6IoWBA/O8azCYhYMf+HEmxCKA36yabGv97XmNxX8aDJPGGYePdXIZQ0lpOWZpP6zRnm2cgOHLD7A84rzXjFlA0uqVhwRjEJzvsnsnlXGl6c9MsioxZsHnQ3eD morten@morten-VirtualBox" >> /home/ubuntu/.ssh/authorized_keys
+
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVcBkZbQP//tgjY/lrk2HxhYiJs8c7PricTWmQDMb9T2qRAhwIw6kAUo0l8fVDZ+NLMGaHfQPY1mBnBUbiWTzNSPaNPlpQryXBon6aMGFAdypK3fjQHi9PgAtqRjqOxpwA8TYIfy6k4gqDyRerlnHmmaK69IBO2Dwjq9G92Cx9lfIQEJhogsaZUInVa090+lZuIHto/3GacOQixYVxeGDLd1X0Y+QGK72T1V2dzbZBu6hXifTVcBjsVFkT4O+6KtxIkOH0mJQBwLNuzF0hydKxNn7aDt37gzYdjz/yRD1PjRs9dN8eeRn4CorzR+PUJOjWW2xQXY/gaeDVbCdAxQed fredros@localhost.localdomain" >> /home/ubuntu/.ssh/authorized_keys
